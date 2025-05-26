3cc19310b175776a9c3dec43cf5da659c6bdeaa7
nriveraaa <202310167@fit.edu.ph>
2025-05-26 11:07:38 +0800

    Feat: Create initial HTML structure for Student List page

---
+++ page/ExamplePage/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Student List</title>
+  <link rel="stylesheet" href="/assets/css/example.css">
+</head>
+<body>
+    
+</body>
+</html>

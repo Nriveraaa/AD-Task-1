f53be1d110a569dabee9f61e0184ace19d630097
nriveraaa <202310167@fit.edu.ph>
2025-05-26 11:07:57 +0800

    Feat: Add navigation bar with links to Home and Results pages

--- page/ExamplePage/index.php
+++ page/ExamplePage/index.php
@@ -6,6 +6,11 @@
   <link rel="stylesheet" href="/assets/css/example.css">
 </head>
 <body>
-    
+    <nav>
+    <ul class="navbar">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="results.php">Results</a></li>
+    </ul>
+  </nav>
 </body>
 </html>

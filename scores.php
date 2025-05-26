a7f9f0b681dc5eb36eebc2d1d85d6c45d8861f61
nriveraaa <202310167@fit.edu.ph>
2025-05-26 11:46:56 +0800

    Feat: Add scores.php file for displaying student subject scores

---
+++ scores.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Student Subject Scores</title>
+  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets/css/scores.css">
+</head>
+<body>
+
+  <h1>Student Scores by Subject</h1>
+  <div id="studentScores"></div>
+
+  <script src="assets/js/scores.js"></script>
+</body>
+</html>
